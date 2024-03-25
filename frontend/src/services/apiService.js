51af522fdf9d4e582f33064a109312ae4d313efd
shamimshihab <shamimshihab56@gmail.com>
2024-03-24 21:50:37 -0700

    update backend hosting address

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
-const API_BASE_URL = "http://localhost:4000";
+const API_BASE_URL = "https://paint-stock-status-app.onrender.com";
+// const API_BASE_URL = "http://localhost:4000";
 
 // Logs in a user
 export const loginUser = async (userName, password) => {
